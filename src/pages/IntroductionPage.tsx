23bb2423a23a6b4ee876e4c4640c70ecd87112b1
Legacy Architect <Craig@legacyarchitectrva.com>
2026-07-14 12:33:31 -0400

    Fix Blob type casting for PDF creation

--- src/pages/IntroductionPage.tsx
+++ src/pages/IntroductionPage.tsx
@@ -363,7 +363,7 @@ export default function IntroductionPage() {
     });
 
     const pdfBytes = await pdfDoc.save();
-    const blob = new Blob([pdfBytes], { type: "application/pdf" });
+    const blob = new Blob([pdfBytes as BlobPart], { type: "application/pdf" });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
